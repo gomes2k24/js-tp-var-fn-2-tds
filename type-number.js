f8ecc528576606a4e8c9392e1914a9d03e4b6c4b
gomes2k24 <160244732+gomes2k24@users.noreply.github.com>
2024-11-22 08:53:03 -0300

    carga_js-tp-var-fn

---
+++ type-number.js
@@ -0,0 +1,23 @@
+//tipo Number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero - segundoNumero;
+console.log(operacaoMatematica)
+
+// ponto flutuante
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemzero = .5;
+
+const novaOperação = primeiroNumero/numeroPontoFlutuante; 
+
+console.log(novaOperação)
+
+//NaN -> Not a number (não é um número)
+ 
+const alura = "Alura"
+console.log(alura*primeiroNumero)
